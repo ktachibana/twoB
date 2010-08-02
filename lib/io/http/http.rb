dbcacfa50696bc0d56794fd3f0e26bf5dc7b8e93
Kenichi Tachibana <deadzone@be.to>
2010-08-03 00:43:26 +0900

    HTTPでエラーリクエストが返ってきたときはraiseするように変更
    レスポンスのステータスコードが400,500番台だったときは、
    ProtocolErrorのメッセージにcodeとmessageをつけてraiseする。

--- lib/io/http/http.rb
+++ lib/io/http/http.rb
@@ -24,6 +24,7 @@ class HTTPGetInput
   def each_read
     HTTP.start(@request.host) do |http|
       http.request_get(@request.path, @request.headers) do |response|
+        raise ProtocolError, "StatusCode:#{response.code} - #{response.message}" if response.code =~ /[45]../
         response.read_body do |data|
           yield data
         end
